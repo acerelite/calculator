5a13cc799efc554e50bcf1938d3d967eeb6fad60
acerelite <61611934+acerelite@users.noreply.github.com>
2023-01-23 18:19:34 +0800

    Complete JS calc logic and Add simple css design

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function add(a, b) {
-  return Math.round((a + b + Number.EPSILON) * 10000000) / 10000000;
+  return Math.round((a + b + Number.EPSILON) * 100000) / 100000;
 }
 function subtract(a, b) {
-  return Math.round((a - b + Number.EPSILON) * 10000000) / 10000000;
+  return Math.round((a - b + Number.EPSILON) * 100000) / 100000;
 }
 function multiply(a, b) {
-  return Math.round((a * b + Number.EPSILON) * 10000000) / 10000000;
+  return Math.round((a * b + Number.EPSILON) * 100000) / 100000;
 }
 function divide(a, b) {
-  return Math.round((a / b + Number.EPSILON) * 10000000) / 10000000;
+  return Math.round((a / b + Number.EPSILON) * 100000) / 100000;
 }
 
 function operate(a, b, operator) {
@@ -29,28 +29,22 @@ function operate(a, b, operator) {
   }
 }
 
-function displayNumber() {
-  if(equation.at(-1) === "=") {
-    equation = [];
-    displayOut.textContent = "0"
+function checkKeybPress(e, numBtn) {
+  if (typeof e === "string") {
+
+    return `${e}`;
   }
-  if (displayOut.textContent === "0" || regex.test(equation.at(-1))) {
-    equation.push(Number(this.textContent));
-    displayOut.textContent = this.textContent;
-    return;
-  }
-  equation.push(Number(this.textContent));
-  displayOut.textContent += this.textContent;
+  return numBtn.textContent;
 }
 
 function completeNumber(equation) {
-  let tempArr = equation.join("").split(regex);
+  let tempArr = equation.join("").split(operatorSymbols);
   if (tempArr.at(-1) === "") {
     tempArr.pop();
     tempArr.push("0");
   }
   for (let unit of equation) {
-    if (unit.toString().match(regex)) {
+    if (unit.toString().match(operatorSymbols)) {
       tempArr.push(unit.toString());
       break;
     }
@@ -58,28 +52,46 @@ function completeNumber(equation) {
   return tempArr;
 }
 
-function operatorSelect() {
-  equation.at(-1) === "=" ? equation.pop(): "";
+function displayNumber(e) {
+  let textContent = checkKeybPress(e, this);
+
+  if (equation.at(-1) === "=") {
+    equation = [];
+    displayOut.textContent = "0";
+  }
+  if (displayOut.textContent === "0" || operatorSymbols.test(equation.at(-1))) {
+    equation.push(Number(textContent));
+    displayOut.textContent = textContent;
+    return;
+  }
+  equation.push(Number(textContent));
+  displayOut.textContent += textContent;
+}
+
+function operatorSelect(e) {
+  let textContent = checkKeybPress(e, this);
   let result = 0;
-  if (equation.at(-1).toString().match(regex)) {
+  equation.at(-1) === "=" ? equation.pop() : "";
+
+  if (equation.at(-1).toString().match(operatorSymbols)) {
     let numFromEquation = Number(
       equation.slice(0, equation.length - 1).join("")
     );
     result = operate(numFromEquation, numFromEquation, equation.at(-1));
     equation = [];
     equation.push(result);
-    equation.push(this.textContent);
+    equation.push(textContent);
     displayOut.textContent = result;
     return;
   }
-  if (equation.join("").split(regex).length === 2) {
+  if (equation.join("").split(operatorSymbols).length === 2) {
     let seperate = completeNumber(equation);
     result = operate(Number(seperate[0]), Number(seperate[1]), seperate[2]);
     equation = [];
     equation.push(result);
     displayOut.textContent = result;
   }
-  equation.push(this.textContent);
+  equation.push(textContent);
 }
 
 function clearCalc() {
@@ -89,13 +101,13 @@ function clearCalc() {
 }
 
 function addDecPoint() {
-  if(equation.at(-1) === "=") {
+  if (equation.at(-1) === "=") {
     equation = [];
     displayOut.textContent = "0.";
     equation.push("0.");
     return;
   }
-  if (regex.test(equation.at(-1))) {
+  if (operatorSymbols.test(equation.at(-1))) {
     displayOut.textContent = "0.";
     equation.push(".");
     return;
@@ -108,7 +120,7 @@ function addDecPoint() {
 }
 
 function backspace() {
-  if(equation.at(-1) === "=") {
+  if (equation.at(-1) === "=") {
     equation = [];
     equation.push[0];
     displayOut.textContent = "0";
@@ -120,7 +132,7 @@ function backspace() {
     equation.pop();
     return;
   }
-  if (regex.test(equation.at(-1))) {
+  if (operatorSymbols.test(equation.at(-1))) {
     equation.pop();
   }
   displayOut.textContent = displayOut.textContent.substring(
@@ -132,7 +144,7 @@ function backspace() {
 
 function calculate() {
   let result = 0;
-  if(equation.at(-1).toString().match(regex)){
+  if (equation.at(-1).toString().match(operatorSymbols)) {
     let numFromEquation = Number(
       equation.slice(0, equation.length - 1).join("")
     );
@@ -144,7 +156,7 @@ function calculate() {
     displayOut.textContent = result;
     return;
   }
-  if (equation.join("").split(regex).length === 2) {
+  if (equation.join("").split(operatorSymbols).length === 2) {
     let seperate = completeNumber(equation);
     result = operate(Number(seperate[0]), Number(seperate[1]), seperate[2]);
     displayOut.textContent = result;
@@ -154,11 +166,14 @@ function calculate() {
   }
 }
 
-const regex = /[\-\/\+\*]/;
+const operatorSymbols = /[\-\/\+\*]/;
+const num = /^\d+$/;
 let equation = [];
 equation.push(0);
 let displayOut = document.querySelector(".displayOut");
 const numberBtns = document.querySelectorAll(".numbers > button");
+
+console.log(numberBtns);
 const operators = document.querySelectorAll(".operators > button");
 const equal = document.querySelector(".equal");
 const clear = document.querySelector(".clear");
@@ -174,3 +189,20 @@ operators.forEach((operator) =>
 clear.addEventListener("click", clearCalc);
 dot.addEventListener("click", addDecPoint);
 equal.addEventListener("click", calculate);
+del.addEventListener("click", backspace);
+
+window.addEventListener("keydown", function (e) {
+
+  console.log(e.key);
+  if (operatorSymbols.test(e.key)) {
+    operatorSelect(e.key);
+  } else if (num.test(e.key)) {
+    displayNumber(e.key);
+  } else if (e.key === "=") {
+    calculate();
+  } else if (e.key === ".") {
+    addDecPoint();
+  } else if (e.key === "Backspace"){
+    backspace();
+  }
+});
